cff4d1f9d487c53ffd6478e6d5ba07cd17170b81
AbdelRahman Alnajjar <>
2022-11-25 19:27:47 -0800

    older-version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
-
 import Navbar from '../components/Navbar'
-import { AuthContextProvider } from '../stores/authContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthContextProvider>
+    <div>
       <Navbar />
       <Component {...pageProps} />
-    </AuthContextProvider>
+    </div>
   )
 }
 
--- stores/authContext.tsx
+++ stores/authContext.tsx
@@ -1,26 +1,26 @@
-import { createContext, useState, useEffect } from 'react'
-import netlifyIdentity from 'netlify-identity-widget'
+// import { createContext, useState, useEffect } from 'react'
+// import netlifyIdentity from 'netlify-identity-widget'
 
-const AuthContext = createContext({
-  user: null,
-  login: () => {},
-  logout: () => {},
-  authReady: false
-})
+// const AuthContext = createContext({
+//   user: null,
+//   login: () => {},
+//   logout: () => {},
+//   authReady: false
+// })
 
-export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+// export const AuthContextProvider = ({ children }) => {
+//   const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    // init netlify identity connection
-    netlifyIdentity.init()
-  }, [])
+//   useEffect(() => {
+//     // init netlify identity connection
+//     netlifyIdentity.init()
+//   }, [])
 
-  return (
-    <AuthContext.Provider value={user}>
-      { children }
-    </AuthContext.Provider>
-  )
-}
+//   return (
+//     <AuthContext.Provider value={user}>
+//       { children }
+//     </AuthContext.Provider>
+//   )
+// }
 
-export default AuthContext
+// export default AuthContext
